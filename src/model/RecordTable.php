6607e5176394ad87db9d9504d0d7c0abd7500005
Mohammad Nadzif <demihuman@live.com>
2018-07-21 05:47:04 +0700

    init active record class

--- src/model/RecordTable.php
+++ src/model/RecordTable.php
@@ -9,7 +9,94 @@
 namespace nadzif\file\model;
 
 
-class RecordTable
+use nadzif\file\File;
+use yii\db\ActiveRecord;
+
+/**
+ * Class RecordTable
+ *
+ * @package nadzif\file\model
+ * @property integer $id
+ * @property string  $type
+ * @property string  $originalName
+ * @property string  $alias
+ * @property string  $path
+ * @property string  $filename
+ * @property double  $size
+ * @property string  $extension
+ * @property string  $mimeType
+ * @property string  $thumbnailFilename
+ * @property double  $thumbnailSize
+ * @property string  $thumbnailExtension
+ * @property string  $thumbnailMimeType
+ * @property string  $additionalInformation
+ * @property string  $createdAt
+ * @property integer $createdBy
+ * @property string  $updatedAt
+ * @property integer $updatedBy
+ * @property string  $deletedAt
+ * @property integer $deletedBy
+ * @property integer $flag
+ */
+class RecordTable extends ActiveRecord
 {
+    const FLAG_INSERTED = 0;
+    const FLAG_RESTORED = 1;
+    const FLAG_UPDATED  = 2;
+    const FLAG_DELETED  = 3;
+
+    const TYPE_IMAGE    = 'image';
+    const TYPE_DOCUMENT = 'document';
+    const TYPE_VIDEO    = 'video';
+    const TYPE_AUDIO    = 'audio';
+    const TYPE_OTHER    = 'other';
+
+
+    const ALIAS_BACKEND  = '@backend';
+    const ALIAS_FRONTEND = '@frontend';
+    const ALIAS_WEB      = '@web';
+
+    public static function tableName()
+    {
+        return '{{%' . File::$tableName . '}}';
+    }
+
+    public function rules()
+    {
+        $rules   = parent::rules();
+        $rules[] = ['size', 'double', 'max' => File::$maximum];
+        return $rules;
+    }
+
+    public function createThumbnail($extension = 'jpg', $replaceExist = true, $filename = null)
+    {
+
+    }
+
+    public function getImagePreview()
+    {
+
+    }
+
+    public function getSource()
+    {
+
+    }
+
+    public function getFullPath()
+    {
+
+    }
+
+    public function deleteFile()
+    {
+
+    }
+
+    public function delete()
+    {
+        return parent::delete();
+    }
+
 
 }
